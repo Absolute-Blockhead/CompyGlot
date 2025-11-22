dc02d5960699c6b105027a43b07bf203726f06c9
Will Blanchard <wpblanchard@proton.me>
2025-11-22 01:08:40 +0000

    First Commit!
    New Features:
    
    None
    
    Incomplete Features
    
    - Loading an entire locale's folder, the file path management logic currently lacks a functional implementation.
    
    Bug Fixes
    
    - Fixed a small bug in the main() function where one of the keys couldn't be found.
    
    Known Bugs
    
    - There is an issue with 'placeables' making it into Rust from the Fluent file. Will inspect for next update.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::borrow::Cow;
 use compyglot::*;
-use fluent_bundle::FluentArgs;
+use fluent_bundle::{FluentArgs, FluentBundle, FluentMessage, FluentResource, FluentValue};
 use std::error::Error;
+use unic_langid::LanguageIdentifier;
 
 //===============\\
 // MAIN FUNCTION \\
@@ -13,7 +15,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let locale = load_translation_file("en-US", Some("standard.ftl"))?;
 
     let identifier: TranslationKey = TranslationKey::new(&locale, "hello-world");
-    let message: TranslationKey = TranslationKey::new(&locale, "will-is-cool");
+    let message: TranslationKey = TranslationKey::new(&locale, "i-am-cool");
 
     let mut arguments: FluentArgs = FluentArgs::new();
     arguments.set("application-name", "Compyglot Test");
@@ -25,5 +27,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", app_info.value());
 
     Ok(())
+
+}//End of Function
+
+fn test_fluent_with_strings() {
+
+    let ftl_string = String::from("hello-world = Hello World!\nintro = Welcome,{$name}.");
+
+    let resource: FluentResource = FluentResource::try_new(ftl_string).expect("Failed to create Fluent resource.");
+    let language_id: LanguageIdentifier = "en-US".parse().expect("Failed to parse language identifier.");
+    let mut bundle = FluentBundle::new(vec![language_id]);
+    bundle.add_resource(resource).expect("Failed to add Fluent resource to the bundle.");
+
+    let message: FluentMessage = bundle.get_message("hello-world").expect("Failed to get message.");
+    let pattern = message.value().expect("Failed to get message.");
+    let value: Cow<str> = bundle.format_pattern(&pattern, None, &mut vec![]);
+
+    assert_eq!(value, "Hello World!");
+    println!("{}", value);
+
+    let mut arguments: FluentArgs = FluentArgs::new();
+    arguments.set("name", FluentValue::from("Will"));
+
+    let message: FluentMessage = bundle.get_message("intro").expect("Failed to get message.");
+    let pattern = message.value().expect("Failed to get message.");
+    let value: Cow<str> = bundle.format_pattern(&pattern, Some(&arguments), &mut vec![]);
+
+    println!("{}", value);
 
 }//End of Function
